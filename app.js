0e596eaa0291778e3d7cb62d923cf40715e0f4ac
ZacharyJohnson <zaj6827@gmail.com>
2017-06-06 13:01:51 -1000

    added lab2 conent to aboutme repository

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict';
+var userAnswer1 = prompt ('Have I ever been to Seattle before?').toLowerCase();
+  if (userAnswer1 == 'yes') {
+    alert('Correct! I was stationed at Fort Lewis and visited.');
+  } else {
+    alert('Incorrect! I was stationed at Fort Lewis and visited.');
+  }
+console.log('User answer to question 1 with userAnswer1');
+var userAnswer2 = prompt ('Have I ever been skydiving?').toLowerCase();
+  if (userAnswer2 == 'yes') {
+    alert('Correct! I went skydiving in California.');
+  } else {
+    alert('Incorrect! I went skydiving in California.');
+  }
+console.log('User answer to question 2 is userAnswer2');
+var userAnswer3 = prompt ('Have I ever been bungie jumping?').toLowerCase();
+  if (userAnswer3 == 'yes') {
+    alert('Correct! I bungie jumped here in Washington!');
+  } else {
+    alert('Incorrect! I bungie jumped here in Washington.');
+  }
+console.log('User answer to question 3 is userAnswer3');
+var userAnswer4 = prompt ('Have I traveled out of the country?').toLowerCase();
+  if (userAnswer4 == 'yes') {
+    alert('Correct! I have traveled to Singapore, Ireland, and Afghanistan.' );
+  } else {
+    alert('Incorrect! I have traveled to Singapore, Ireland, and Afghanistan.');
+  }
+  console.log('User answer to question 4 is userAnswer4');
+  var userAnswer5 = prompt ('Did I learn if/else statements better because of this?').toLowerCase();
+    if (userAnswer5 == 'yes') {
+      alert('Correct! I also learned how to properly place the .toUpperCase and .toLowerCase!');
+    } else {
+      alert('Incorrect! This was very good practice for things to come.');
+    }
+  console.log('User answer to question 5 is userAnswer5');
